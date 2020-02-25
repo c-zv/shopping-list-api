1b7bd08eb4dc8278f3040b808debaf9f8562f0a1
S-4-S <zv.const@gmail.com>
2020-02-25 17:10:08 +0000

    #1 - Add User model

---
+++ db/migrate/20200225154754_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+
+      t.timestamps
+    end
+
+    add_reference :shopping_lists, :user, null: false, foreign_key: true
+  end
+end
