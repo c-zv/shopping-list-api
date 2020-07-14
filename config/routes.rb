09987c54234197995efb044b5c9bd55e68014bc4
c-zv <zv.const@gmail.com>
2020-07-14 17:43:58 +0100

    Add endpoint to get a shopping list category

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :shopping_lists do 
       resources :items, only: [:create, :update, :destroy], controller: 'shopping_list_items'
     end
-    resources :categories, only: [:index], controller: 'shopping_list_categories'
+    resources :categories, only: [:index, :show], controller: 'shopping_list_categories'
   end
 end
