a1018d3ea51f5463ca2abfbc3ed77ba05b436842
c-zv <zv.const@gmail.com>
2020-07-14 17:46:15 +0100

    Use items in shopping list always ordered by created date

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,6 +1,6 @@
 class ShoppingList < ApplicationRecord
   belongs_to :user
-  has_many :items, class_name: 'ShoppingListItem', foreign_key: 'shopping_list_id', dependent: :destroy
+  has_many :items, -> { order(created_at: :asc)}, class_name: 'ShoppingListItem', foreign_key: 'shopping_list_id', dependent: :destroy
   belongs_to :category, class_name: 'ShoppingListCategory', foreign_key: 'shopping_list_category_id'
 
   validates :category, presence: true
