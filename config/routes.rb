f4d21741f6ba04921dfc604ad7c4df27d010098e
c-zv <zv.const@gmail.com>
2020-07-21 12:05:25 +0100

    Define routes for store products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace 'api' do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :store_products, only: [:index, :show]
     resources :shopping_lists do 
       resources :items, only: [:create, :update, :destroy], controller: 'shopping_list_items'
     end
